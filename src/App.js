4fde7e7ae79c8c4890814ef44bb7b6b68277e250
loifav <favrelc22@gmail.com>
2024-11-16 01:00:45 +0100

    test ci-cd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Git Action
+          Learn Git Action V2
         </a>
       </header>
     </div>
